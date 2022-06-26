3ac6183b817c3b09cff22a24570354af538e2941
jun.ma <juning.ma@qq.com>
2022-06-26 17:07:21 +0800

    [Feature] add nginx proxy

--- spot-oss/src/test/java/github/beeclimb/spot/oss/SpotOssApplicationTests.java
+++ spot-oss/src/test/java/github/beeclimb/spot/oss/SpotOssApplicationTests.java
@@ -1,13 +1,16 @@
 package github.beeclimb.spot.oss;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpotOssApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+    }
 
 }
